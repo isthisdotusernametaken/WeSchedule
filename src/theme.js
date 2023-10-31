ce22f7f0d1605b2bfadee6279e6ad752689e9f64
isthisdotusernametaken <92243150+isthisdotusernametaken@users.noreply.github.com>
2023-10-30 17:29:37 -0700

    Added CSS theme change behavior in theme.js

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+/*
+    Exports: THEMES (object), setTheme (function)
+    The THEMES object can be externally viewed to select a theme. One of its
+    keys (not properties - to simplify external selection code) can then be
+    provided to setTheme to replace the page's stylesheet.
+*/
+
+// These objects are used only as immutable dictionaries, and property naming
+// conventions are ignored to simplify the code that sets the theme.
+const THEMES_DICT = Object.freeze({
+    "Dark 1": ["Vapor", "https://bootswatch.com/5/vapor/bootstrap.min.css"],
+    "Dark 2": ["Darkly", "https://bootswatch.com/5/darkly/bootstrap.min.css"],
+    "Light 1": ["Pulse", "https://bootswatch.com/5/pulse/bootstrap.min.css"],
+    "Light 2": ["Sandstone", "https://bootswatch.com/5/sandstone/bootstrap.min.css"],
+});
+export const THEMES = Object.freeze(Object.fromEntries(
+    Object.entries(THEMES_DICT).map(
+        ([label, [name, _]]) => [label, name]
+    ) // Avoiding a list here makes THEMES truly immutable
+));
+
+export const setTheme = (theme) => {console.log(theme);console.log(THEMES_DICT)
+    if (!Object.keys(THEMES_DICT).includes(theme))
+        return false;
+
+    document.getElementById("css-theme").href = THEMES_DICT[theme][1];
+    return true;
+}

cf5ce56fdb942d5cdfae1ec9537fcfd42071a893
Kraily4t8 <tomtien2002@outlook.com>
2023-11-16 21:32:10 -0800

    Day component
    Displays day number, day of the week, and symbols to indicate importance level

---
+++ src/components/Day.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css'
+
+function Day() {
+    //get current and display day number and day of the week//
+    //display level of importance
+    return (
+      <div>
+        <h2>1   Wednesday   important/notice/absent</h2>
+      </div>
+    );
+  }
+
+export default Day;

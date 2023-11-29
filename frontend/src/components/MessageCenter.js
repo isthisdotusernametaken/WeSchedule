b635ab5f2bd45eac49ce6007fbc6bf36c6ae7bf6
calebkrauter <42187569+calebkrauter@users.noreply.github.com>
2023-11-29 12:18:26 -0800

    Update Msg center UI and add input field

--- frontend/src/components/MessageCenter.js
+++ frontend/src/components/MessageCenter.js
@@ -3,17 +3,19 @@ import { THEMES, setTheme } from "../theme.js";
 function MessageCenter() {
     return (
         <msgFrame className="border border-success p-2 mb-2" data-bs-theme="dark">
-            <div className="container">
-                <div className="row inner-frame">
+            <div className="container inner-frame p-1 col-sm-4 col-md-6 col-lg-6 float-start border bg-dark">
+                <div className="row">
                     {/* 12 avaliable col */}
-                    <div className="col-sm-4 col-md-6 justifiy-content-center order-1">
-                        <div className="p-0.5 border bg-dark">Hey</div>
+                    <div className="order-1 justify-content-start col-sm-4">
+                        <div className="message-incoming p-0.5">OTHER</div>
                     </div>
-                    <div className="col-sm-4 col-md-6 justifiy-content-center order-2">
-                        <div className="p-0.5 border bg-dark">Hey</div>
+                    <div className="justify-content-end order-2 col-sm-4 d-flex flex-row-reverse">
+                        <div className="message-outgoing p-0.5">ME</div>
                     </div>
                 </div>
-
+                <div className="row">
+                    <input type="textfield" className="chat-input"></input>
+                </div>
             </div>
             
         </msgFrame>

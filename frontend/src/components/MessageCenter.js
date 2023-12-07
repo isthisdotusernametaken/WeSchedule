840107de9b193be07777dd0367d4bab4c443070b
Caleb Krauter <krautercaleb@gmail.com>
2023-12-07 11:51:29 -0800

    Update scroll ability in message center and implement more dynamic code.

--- frontend/src/components/MessageCenter.js
+++ frontend/src/components/MessageCenter.js
@@ -1,54 +1,53 @@
 import React, { useEffect, useRef, useState } from "react";
 import { THEMES, setTheme } from "../theme.js";
 import { message } from "statuses";
-function MessageCenter() {
+function MessageCenter({user}) {
 
     // Use state hooks to store the messages and the current message being sent.
-    const [corespondence, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
-    const latestMessage = useRef(null);
+    const [correspondence, setMessages] = useState([]);
+    const inputField = useRef(null);
+    const latestMsg = useRef(null);
 
     useEffect(() => {
-        if (corespondence.length === 0) {
-            document.getElementById("my-message-box-out").style.display = "none";
-            document.getElementById("my-message-box-in").style.display = "none";
+        if (latestMsg.current != null) {
+            latestMsg.current.scrollTop = latestMsg.current.scrollHeight;
         }
-    }); 
+    }, [correspondence]);
+
     const send = () => {
-        document.getElementById("my-message-box-out").style.display = "block";
-        document.getElementById("my-message-box-in").style.display = "block";
-        
-        if (newMessage.trim() === "") {
+        let message = inputField.current.value.trim();
+        if (message === "") {
             console.log("No message sent.")
-            return;
         } else {
-            setMessages([...corespondence, {text: newMessage, sender: "YOU"}]);
-            console.log(newMessage);
-            const newMessageBubble = (
-                `<div>User: ${newMessage}</div>`
-            );
-            document.getElementById("my-message-box-out").innerHTML += newMessageBubble + `<div><br></div>`;
-            document.getElementById("my-message-box-in").innerHTML += newMessageBubble + `<div><br></div>`;
-            latestMessage.current.scrollIntoView({ behavior: 'smooth' });
-            document.getElementById("input-msg-field").value = "";
+            setMessages([...correspondence, {user: user, time: new Date(),content: message}]);
+            inputField.current.value = "";
         }
     }
 
     return (
-        <msgFrame className="border border-success p-2 mb-2" data-bs-theme="dark">
-            <div className="container inner-frame p-1 col-sm-4 col-md-6 col-lg-6 float-start border bg-dark">
-                <div id="my-message-box-out" className="message-box message-left p-3 message-outgoing p-0.5"></div>
-                <div ref={latestMessage} id="my-message-box-in" className="message-box message-right p-3 message-incoming p-0.5"></div>
-
-                <div className="row">
-                    <input type="textfield" id="input-msg-field" className="chat-input" onChange={(e) => setNewMessage(e.target.value)}></input>
+        <div className="border border-success p-2 mb-2" data-bs-theme="dark">
+            <div className="container inner-frame p-1 d-flex flex-column border bg-dark">
+                <div ref={latestMsg} className="h-100 overflow-auto">
+                    {correspondence.map(MessageBubble)}
+                </div>
+                
+            </div>
+                <div className="">
+                    <input ref={inputField} type="textfield" id="input-msg-field" className="chat-input"></input>
                     <button onClick={send}>+</button>
                 </div>
-            </div>
-            
-        </msgFrame>
+        </div>
     );
 }
 
+const MessageBubble = ({
+    user,
+    time,
+    content
+}) => 
+    <div className="rounded-2 p-1 my-4 message-box message-left p-3 message-outgoing">
+        <p>{user}: {time.toString()}</p>
+        <p>{content}</p>
+    </div>
 
 export default MessageCenter;

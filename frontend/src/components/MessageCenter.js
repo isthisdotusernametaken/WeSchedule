654450ed5b3e128475997c1640a5954c1c79b88b
Caleb Krauter <krautercaleb@gmail.com>
2023-12-07 15:31:00 -0800

    Minor Update to style

--- frontend/src/components/MessageCenter.js
+++ frontend/src/components/MessageCenter.js
@@ -67,7 +67,7 @@ const MessageBubble = ({
     time,
     content
 }) => 
-    <div className="rounded-3 p-1 my-4 message-box message-left p-1 message-outgoing">
+    <div className="rounded-4 p-1 my-4 message-box message-left p-1 message-outgoing">
         <p>{user}: <p>{content}</p> </p>
         <div className="rounded-3 p-1 time">
             <p>{time.toString()}</p>

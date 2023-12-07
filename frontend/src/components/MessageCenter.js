23a87ab8767021cc881ea7638870e109396f06d8
Caleb Krauter <krautercaleb@gmail.com>
2023-12-06 18:43:28 -0800

    Update message center to append new messages and delete from input field.

--- frontend/src/components/MessageCenter.js
+++ frontend/src/components/MessageCenter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { THEMES, setTheme } from "../theme.js";
 import { message } from "statuses";
 function MessageCenter() {
@@ -6,30 +6,42 @@ function MessageCenter() {
     // Use state hooks to store the messages and the current message being sent.
     const [corespondence, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
+    const latestMessage = useRef(null);
 
+    useEffect(() => {
+        if (corespondence.length === 0) {
+            document.getElementById("my-message-box-out").style.display = "none";
+            document.getElementById("my-message-box-in").style.display = "none";
+        }
+    }); 
     const send = () => {
+        document.getElementById("my-message-box-out").style.display = "block";
+        document.getElementById("my-message-box-in").style.display = "block";
+        
         if (newMessage.trim() === "") {
             console.log("No message sent.")
             return;
         } else {
-            setMessages([...corespondence, {text: newMessage, sender: "YOU"}])
+            setMessages([...corespondence, {text: newMessage, sender: "YOU"}]);
+            console.log(newMessage);
+            const newMessageBubble = (
+                `<div>User: ${newMessage}</div>`
+            );
+            document.getElementById("my-message-box-out").innerHTML += newMessageBubble + `<div><br></div>`;
+            document.getElementById("my-message-box-in").innerHTML += newMessageBubble + `<div><br></div>`;
+            latestMessage.current.scrollIntoView({ behavior: 'smooth' });
+            document.getElementById("input-msg-field").value = "";
         }
     }
 
     return (
         <msgFrame className="border border-success p-2 mb-2" data-bs-theme="dark">
             <div className="container inner-frame p-1 col-sm-4 col-md-6 col-lg-6 float-start border bg-dark">
+                <div id="my-message-box-out" className="message-box message-left p-3 message-outgoing p-0.5"></div>
+                <div ref={latestMessage} id="my-message-box-in" className="message-box message-right p-3 message-incoming p-0.5"></div>
+
                 <div className="row">
-                    {/* 12 avaliable col */}
-                    <div className="order-1 justify-content-start col-sm-4">
-                        <div className="message-incoming p-0.5">OTHER</div>
-                    </div>
-                    <div className="justify-content-end order-2 col-sm-4 d-flex flex-row-reverse">
-                        <div className="message-outgoing p-0.5">ME: {newMessage.text}</div>
-                    </div>
-                </div>
-                <div className="row">
-                    <input type="textfield" className="chat-input" onChange={(e) => setNewMessage(e.target.value)}></input>
+                    <input type="textfield" id="input-msg-field" className="chat-input" onChange={(e) => setNewMessage(e.target.value)}></input>
                     <button onClick={send}>+</button>
                 </div>
             </div>
